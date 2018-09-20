9941b0693536440890f6122c86ab84023aae4007
ProjetosIoT <43098826+projetosiot@users.noreply.github.com>
2018-09-20 20:33:07 -0300

    Update Exercicio03.ino

--- Exercicio03.ino
+++ Exercicio03.ino
@@ -1,6 +1,8 @@
-/*  Software para controle do Tempo de Pisca do LED */
+/* Exercicio 03
+IoT na Prática com o ESP8266
+Software para controle do Tempo de Pisca do LED */
 
-float tempo = 0;        //Valor Tempo do led 
+int tempo = 0;        //Valor Tempo do led 
 int sensorPin = A0;     //Definindo A0 Entrada Analógica       
 const int ledPin = D4;   // D4 = GPIO2 LEDBuiltin
 int analogValue = 0 ;   //Inicializa Variável Valor Entrada Analógica
@@ -15,14 +17,23 @@ void loop() {
   Serial.print("Valor LDR em % =");      //Print Valor LDR em Porcentagem na Serial
   Serial.println(analogValue*100.0/1023.0);  //Print Valor em % da Entrada Analógica LDR na Serial
   analogValue = analogRead(sensorPin);   //Lendo Entrada Analógica
-  tempo = map(analogValue, 0, 1023, 0.0, 2000.0);   //Converte e atribui para a variavel "tempo" o valor lido no LDR 
-  Serial.print("Valor tempo em seg =");      //Print Valor LDR em Porcentagem na Serial
-  Serial.println(tempo/1000.0);  //Print Valor em % da Entrada Analógica LDR na Serial     
-  analogWrite(ledPin, LOW);    //Liga LED
-  delay(tempo);                //Tempo Ligado
-  analogWrite(ledPin,HIGH);    //Desliga LED
-  delay(tempo);               //Tempo Desligado
-
+  
+  tempo = map(analogValue, 0, 1023, 0, 2000);   //Converte e atribui para a variavel "tempo" o valor lido no LDR 
+  
+  Serial.print("Valor tempo em mseg =");      //Print Valor LDR em Porcentagem na Serial
+  Serial.println(tempo);  //Print Valor em % da Entrada Analógica LDR na Serial  
+  
+  if (tempo>10) { 
+      digitalWrite(ledPin, LOW);   //Liga LED
+      delay(tempo);                //Tempo Ligado  
+      digitalWrite(ledPin,HIGH);   //Desliga LED
+      delay(tempo);                //Tempo Desligado
+  } else {
+      digitalWrite(ledPin,HIGH);    //Desliga LED
+  }
+ 
+  
 }
+
 
  
